7db123b10cecf4c8f0d8b80f929c8de1c33d35a3
XiaoYunhuan <xiao_yunhuan@126.com>
2019-09-04 14:00:10 +0800

    参数接受

--- src/controller/AnnotateHelloController.java
+++ src/controller/AnnotateHelloController.java
@@ -10,8 +10,13 @@ package controller;
  * 注意：本内容仅限于内部传阅，禁止外泄以及用于其他的商业目
  */
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import pojo.User;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,14 +30,83 @@ import javax.servlet.http.HttpServletResponse;
  * 注意：本内容仅限于公司内部传阅，禁止外泄以及用于其他商业目的
  **/
 @Controller
-@RequestMapping("yltx")
 public class AnnotateHelloController {
 
     @RequestMapping("/annotateHello")
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-        ModelAndView mav = new ModelAndView("index.jsp");
+        ModelAndView mav = new ModelAndView("index");
         mav.addObject("message", "annotateHello Spring MVC");
         return mav;
+    }
+
+
+    @RequestMapping("/form")
+    public ModelAndView form(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        return new ModelAndView("form");
+    }
+
+
+    @RequestMapping("/param")
+    @ResponseBody
+    public String getParam(HttpServletRequest request, HttpServletResponse response) {
+        String userName = request.getParameter("userName");
+        String password = request.getParameter("password");
+        System.out.println(userName);
+        System.out.println(password);
+        return "success";
+    }
+
+    @RequestMapping("/param1")
+    @ResponseBody
+    public String getParam1(String userName, String password) {
+        System.out.println(userName);
+        System.out.println(password);
+        return "success";
+    }
+
+    @RequestMapping("/param2")
+    @ResponseBody
+    public String getParam2(@RequestParam("userName") String userName, @RequestParam("password")String password) {
+        System.out.println(userName);
+        System.out.println(password);
+        return "success";
+    }
+
+    @RequestMapping("/param3")
+    @ResponseBody
+    public String getParam3(User user) {
+        System.out.println(user.getUserName());
+        System.out.println(user.getPassword());
+        return "success";
+    }
+
+    @RequestMapping("/value")
+    public ModelAndView handleRequestValue(HttpServletRequest request,
+                                      HttpServletResponse response) {
+        request.setAttribute("message","成功！");
+        return new ModelAndView("test1");
+    }
+
+
+    @ModelAttribute
+    public void model(Model model) {
+        model.addAttribute("message", "注解成功");
+    }
+
+    @RequestMapping("/value1")
+    public String handleRequest() {
+        return "test1";
+    }
+
+    @RequestMapping("/jump")
+    public ModelAndView jump() {
+        ModelAndView mav = new ModelAndView("redirect:/hello");
+        return mav;
+    }
+
+    @RequestMapping("/jump1")
+    public String jump1() {
+        return "redirect:./hello";
     }
 
 }
---
+++ src/pojo/User.java
@@ -0,0 +1,41 @@
+package pojo;
+/*
+ * @Title: User
+ * @Package: pojo
+ * @Description:
+ * @author: xiaoyh
+ * @date: 2019/9/4 13:54
+ * @version: V1.0
+ * @Copyright: 2019 www.chinayltx.com Inc. All rights reserved.
+ * 注意：本内容仅限于内部传阅，禁止外泄以及用于其他的商业目
+ */
+
+/**
+ * @ClassName : User
+ * @Description :
+ * @Author : xiaoyh
+ * @Date : 2019/9/4 13:54
+ * @Copyright: 2019 www.chinayltx.com Inc. All rights reserved
+ * 注意：本内容仅限于公司内部传阅，禁止外泄以及用于其他商业目的
+ **/
+public class User {
+
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
